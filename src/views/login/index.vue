6709729751088f2b32cf18dc62aad28c63d689d0
RainTune <969723165@qq.com>
2020-08-11 08:50:26 +0800

    新增reset.css

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">登录页面</div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+.login {
+}
+</style>
